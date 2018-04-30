303feef65922954483a48c5887c1e2cc841ab5d0
Thomas Hubert <mr.thomas.hubert@gmail.com>
2018-04-30 23:38:04 +0200

    quete relation entities

--- src/AppBundle/Entity/Site.php
+++ src/AppBundle/Entity/Site.php
@@ -32,6 +32,11 @@ class Site
      */
     private $departures;
 
+    /**
+     * @ORM\OneToMany(targetEntity="AppBundle\Entity\Flight", mappedBy="arrival")
+     */
+    private $arrivals;
+
 
     /**
      * @var int
@@ -247,5 +252,39 @@ class Site
     public function getDepartures()
     {
         return $this->departures;
+    }
+
+    /**
+     * Add arrival
+     *
+     * @param \AppBundle\Entity\Flight $arrival
+     *
+     * @return Site
+     */
+    public function addArrival(\AppBundle\Entity\Flight $arrival)
+    {
+        $this->arrivals[] = $arrival;
+
+        return $this;
+    }
+
+    /**
+     * Remove arrival
+     *
+     * @param \AppBundle\Entity\Flight $arrival
+     */
+    public function removeArrival(\AppBundle\Entity\Flight $arrival)
+    {
+        $this->arrivals->removeElement($arrival);
+    }
+
+    /**
+     * Get arrivals
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getArrivals()
+    {
+        return $this->arrivals;
     }
 }
